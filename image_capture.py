b112c0fe04b290e5a911bbb7dc7806cb82d8ec3f
Karthikeyu <karthikeyuboosa@gmail.com>
2020-04-17 03:27:22 +0530

    Refactoring code using image utils file Co-authored-by: shravanikatte <shravanikatte123@gmail.com>

--- image_capture.py
+++ image_capture.py
@@ -2,13 +2,10 @@
 import numpy as np
 import cv2
 import os
+import imagePreprocessingUtils as ipu
 
-IMG_SIZE = 128
 CAPTURE_FLAG = False
 
-#ROI
-start = (450,75)
-end = (800,425)
 
 directory = input('Enter dataset directory name: ')
 
@@ -39,7 +36,7 @@ count = 0
 while(True):
     (t,frame) = camera.read()
     frame = cv2.flip(frame,1)
-    cv2.rectangle(frame,start, end,(0,255,0),2 )
+    cv2.rectangle(frame,ipu.START, ipu.END,(0,255,0),2 )
     pressedKey = cv2.waitKey(1)
     if pressedKey == 27:
         break
@@ -54,13 +51,11 @@ while(True):
         #for count in range(0,500):
     if(CAPTURE_FLAG):
         if(count<1200):
-            roi = frame[ start[1]+5:end[1], start[0]+5:end[0]]
-            #roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-            #roi_gray = cv2.GaussianBlur(roi_gray, (7,7),0)
+            roi = frame[ ipu.START[1]+5:ipu.END[1], ipu.START[0]+5:ipu.END[0]]
             cv2.imshow("Gesture", roi)
             frame = cv2.putText(frame, 'Capturing..', (50,70), cv2.FONT_HERSHEY_SIMPLEX,  
                    1.5, (0,255,0), 2, cv2.LINE_AA)
-            roi = cv2.resize(roi, (IMG_SIZE,IMG_SIZE))
+            roi = cv2.resize(roi, (ipu.IMG_SIZE,ipu.IMG_SIZE))
             cv2.imwrite("%s/%d.jpg"%(path,count), roi)
             count +=1
             print(count)
